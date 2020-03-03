9b7f537ba6dd5896a0157986fec7c2b836afcb93
paul-laird <35932444+paul-laird@users.noreply.github.com>
2020-03-03 10:01:34 +0000

    Create a.py

---
+++ a.py
@@ -0,0 +1,16 @@
+myGlobal=5
+myMaskedVariable=9
+
+def myF():
+    myLocal=3
+#    global myMaskedVariable
+    myMaskedVariable=7
+    
+myF()
+
+def printVars():
+    print(myGlobal)
+#    print(myLocal)
+    print(myMaskedVariable)
+    
+printVars()    
